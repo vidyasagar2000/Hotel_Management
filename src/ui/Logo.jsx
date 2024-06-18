400a6153fe3ed7166228750204e52afc2038b30b
unknown <vidya9955297836@gmail.com>
2024-06-18 15:22:15 +0530

    Added DarkMode TOggle Feature to our application

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useDarkMode } from "../context/DarkModeContext";
 
 const StyledLogo = styled.div`
   text-align: center;
@@ -9,10 +10,20 @@ const Img = styled.img`
   width: auto;
 `;
 
+const ImageText = styled.p`
+  letter-spacing: 2px;
+  font-family: monospace;
+`;
+
 function Logo() {
+  const { isDarkMode } = useDarkMode();
+
+  const src = isDarkMode ? "/Hotel-Logo.png" : "/Hotel-Logo.png";
+
   return (
     <StyledLogo>
-      <Img src="/Hotel-Logo.png" alt="Logo" />
+      <Img src={src} alt="Logo" />
+      <ImageText>THE ROYAL PALACE</ImageText>
     </StyledLogo>
   );
 }
